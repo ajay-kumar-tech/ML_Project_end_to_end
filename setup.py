7832ee98ff962b436fefa108a9071476d42bbbc5
Ajay Kumar <apmotivation3@gmail.com>
2025-01-12 16:08:48 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+# it is resposible to create ml apllication as package 
+
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this funcation will return list of requirement
+    '''
+
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+    name="mlproject",
+    version='0.0.1',
+    author="Ajay Kumar",
+    author_email="apmotivation3@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirement.txt")
+)
